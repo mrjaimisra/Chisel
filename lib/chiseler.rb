c81775f593d45958c6f35b6c6e27b66a123e82a3
mrjaimisra <misrajai01@gmail.com>
2015-06-11 14:28:06 -0600

    I added a puts statement at the bottom to show the functionality

--- lib/chiseler.rb
+++ lib/chiseler.rb
@@ -32,3 +32,18 @@ attr_reader :chunks, :listed_chunks
   end
 
 end
+
+chi = Chiseler.new("# My Life in Desserts
+
+## Chapter 1: The Beginning
+
+\"You just *have* to try the cheesecake,\" he said. \"Ever since it appeared in
+**Food & Wine** this place has been packed every night.\"
+
+My favorite cuisines are:
+
+* Sushi
+* Barbeque
+* Mexican")
+
+puts chi.emphasize
